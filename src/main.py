baf8b9feeb843cd4a508e64a771cda862418b85e
andy6491 <andyblackburn9@gmail.com>
2025-06-03 23:23:35 -0400

    generate pages recursively

--- src/main.py
+++ src/main.py
@@ -46,7 +46,7 @@ def generate_page(from_path, template_path, dest_path):
     replace_content = replace_title.replace("{{ Content }}", markdown_conversion)
     os.makedirs(os.path.dirname(dest_path), exist_ok=True)
     with open(dest_path, "w") as f:
-        dest_write = f.write(replace_content)
+        f.write(replace_content)
 
 def copy_public(static, public):
     if os.path.exists(public):
@@ -55,7 +55,29 @@ def copy_public(static, public):
             shutil.copytree(static, public)
 
 
-  
+def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
+    items = os.listdir(dir_path_content)
+    for item in items:
+        source_path = os.path.join(dir_path_content, item)
+        content_relative_path = source_path[len(dir_path_content):]
+        if content_relative_path.startswith("/"):
+            content_relative_path = content_relative_path[1:]
+        html_relative_path = content_relative_path.replace(".md", ".html")
+        dest_path = os.path.join(dest_dir_path, html_relative_path)
+        if os.path.isfile(source_path) and source_path.endswith(".md"):
+            create_dir = os.path.dirname(dest_path)
+            os.makedirs(create_dir, exist_ok=True)
+            with open(source_path, "r") as f:
+                source_read = f.read()
+            with open(template_path, "r") as f:
+                template_read = f.read()
+            markdown_conversion = markdown_to_html_node(source_read).to_html()
+            replace_content = template_read.replace("{{ Content }}", markdown_conversion)
+            with open(dest_path, "w") as f:
+                f.write(replace_content)
+        elif os.path.isdir(source_path):
+            os.makedirs(os.path.join(dest_dir_path, item), exist_ok=True)
+            generate_pages_recursive(source_path, template_path, os.path.join(dest_dir_path, item))
 
 
 
@@ -63,7 +85,7 @@ def main():
     node = TextNode("this is some anchor text", TextType.LINK, "https://www.boot.dev")
     print(node)
     copy_public("static", "public")
-    generate_page("content/index.md", "template.html", "public/index.html")
+    generate_pages_recursive("content", "template.html", "public")
 
 
 
